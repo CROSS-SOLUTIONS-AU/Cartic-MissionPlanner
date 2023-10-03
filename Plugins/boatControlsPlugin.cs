58bf47cb87df92569aae945fd81262d3e760d42d
JPGrizzl <37286031+JPGrizzl@users.noreply.github.com>
2023-10-03 14:59:17 +0930

    test record button

--- Plugins/boatControlsPlugin.cs
+++ Plugins/boatControlsPlugin.cs
@@ -19,7 +19,16 @@ namespace cartic
 {
     public class BoatControlsPlugin : MissionPlanner.Plugin.Plugin
     {
-        private bool recording = false;
+        private bool _recording = false;
+        public bool Recording
+        {
+            get { return _recording; }
+            private set
+            {
+                Console.WriteLine($"Recording set to {value}. Called from {new StackTrace().GetFrame(1).GetMethod().Name}");
+                _recording = value;
+            }
+        }
         private Control recButton = new MissionPlanner.Controls.MyButton();
         public override string Name
         {
@@ -55,7 +64,7 @@ namespace cartic
 
         public override bool Loaded()
         {
-            loopratehz = 1;
+            loopratehz = 0.5f;
             
             return true;
         }
@@ -63,8 +72,20 @@ namespace cartic
 
         public override bool Loop()
         {
-            string recordingState = recording ? "RcrdDpth:1" : "RcrdDpth:0";
-            SendMessage(recordingState);
+            if (Recording)
+            {
+                Console.WriteLine("Recording: " + Recording.ToString());
+                recButton.Text = "End Recording";
+                string recordingState = "RcrdDpth:1";
+                SendMessage(recordingState);
+            }
+            else
+            {
+                Console.WriteLine("Recording: " + Recording.ToString());
+                recButton.Text = "Start Recording Soundings";
+                string recordingState = "RcrdDpth:0";
+                SendMessage(recordingState);
+            }
             return true;
         }
 
@@ -75,10 +96,20 @@ namespace cartic
 
         private void RecordingButtonClick(Object sender, EventArgs e)
         {
-            recording = !recording;
-            if(recording) {
+            Console.WriteLine($"RecordingButtonClick fired. Sender: {sender.GetType().Name}, EventArgs: {e.GetType().Name}");
+
+            if (e is MouseEventArgs mouseEvent)
+            {
+                Console.WriteLine($"Mouse button clicked: {mouseEvent.Button}");
+            }
+
+            Recording = !Recording; // Toggle the recording state
+
+            if (Recording)
+            {
                 recButton.Text = "End Recording";
-            } else
+            }
+            else
             {
                 recButton.Text = "Start Recording Soundings";
             }
@@ -90,29 +121,32 @@ namespace cartic
             {
                 // Get current MAV state
                 MAVState mavState = MainV2.comPort.MAV;
-
-                byte[] encodedText = Encoding.ASCII.GetBytes(message);
+                int hostsysId = Host.comPort.sysidcurrent;
+                int hostcompId = Host.comPort.compidcurrent;
+                byte[] encodedText = Encoding.UTF8.GetBytes(message);
 
                 // Create the message
                 mavlink_statustext_t statusText = new mavlink_statustext_t
-                {
-                    severity = (byte)MAV_SEVERITY.MAV_SEVERITY_INFO,
+                { 
+                    severity = (byte)MAV_SEVERITY.INFO,
                     text = encodedText
                 };
 
                 // Send the message
-                MainV2.comPort.sendPacket(statusText, mavState.sysid, mavState.compid);
+                //MainV2.comPort.sendPacket(statusText, hostsysId, hostcompId);
+                MainV2.comPort.send_text((byte)MAV_SEVERITY.INFO, message);
             }
         }
 
         public void ComPort_RecieveState(object sender, MAVLink.MAVLinkMessage e)
-        {
+        {/*
             if(e.msgid == (uint)MAVLink.MAVLINK_MSG_ID.RANGEFINDER)
             {
                 var depth = (MAVLink.mavlink_rangefinder_t)e.data;
                 recording = true;
                 Console.WriteLine("Depth: " + depth.ToString());
             }
+            */
         }
     }
 }
